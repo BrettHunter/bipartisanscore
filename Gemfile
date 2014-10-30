75eaf02fd5dcdda7d56c366c2de5d5346f57a51e
BrettHunter <bfh935@yahoo.com>
2014-10-30 21:00:46 +0000

    added static page

--- Gemfile
+++ Gemfile
@@ -12,7 +12,7 @@ gem 'pg', '0.15.1'
 gem 'sass-rails', '4.0.2'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
-
+gem 'high_voltage', '~> 2.2.1'
 # Use CoffeeScript for .js.coffee assets and views
 gem 'coffee-rails', '~> 4.0.0'
 
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Bipartisanscore::Application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
   root  'static_pages#index'
   
+  
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
