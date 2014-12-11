ba25c9f99d7997fcccd2bc4e8f75282d98051f4c
BrettHunter <bfh935@yahoo.com>
2014-12-11 21:15:50 +0000

    refactor leg populator

--- lib/tasks/legislator_populator.rb
+++ lib/tasks/legislator_populator.rb
@@ -1,21 +1,38 @@
- 
+def filter(input)
+  if input.title.include? "Del"
+    return true
+  elsif input.title.include? "Com"
+    return true
+  else
+    return false
+  end
+end
+
+
 count_object = Legislator.get_legislators(1)
   count = Legislator.get_count(count_object)
   i = 0
   until i == count do    
   leg = Legislator.get_legislators(i)
-  input = leg.results.first  
-  record = Legislator.find_by bioguide_id: input.bioguide_id
-  if record != nil
-    record.update(title: input.title, first_name: input.first_name, last_name: input.last_name, party: input.party, state: input.state, 
-      bioguide_id: input.bioguide_id.downcase,  chamber: input.chamber, in_office: input.in_office, gender: input.gender, 
-      website: input.website, term_start: input.term_start, term_end: input.term_end)
-  else 
-    Legislator.create(title: input.title, first_name: input.first_name, last_name: input.last_name, party: input.party, 
-      state: input.state, bioguide_id: input.bioguide_id.downcase, chamber: input.chamber, in_office: input.in_office, 
-      gender: input.gender, website: input.website, term_start: input.term_start, term_end: input.term_end)
+  input = leg.results.first 
+    
+  if filter(input) == false
+    record = Legislator.find_by bioguide_id: input.bioguide_id
+    if record != nil
+      record.update(title: input.title, first_name: input.first_name, last_name: input.last_name, party: input.party, state: input.state, 
+        bioguide_id: input.bioguide_id.downcase,  chamber: input.chamber, in_office: input.in_office, gender: input.gender, 
+        website: input.website, term_start: input.term_start, term_end: input.term_end)
+    else 
+      Legislator.create(title: input.title, first_name: input.first_name, last_name: input.last_name, party: input.party, 
+        state: input.state, bioguide_id: input.bioguide_id.downcase, chamber: input.chamber, in_office: input.in_office, 
+        gender: input.gender, website: input.website, term_start: input.term_start, term_end: input.term_end)
+    end
   end
     i +=1
     puts i
   end
-    
+ 
+
+
+  
+  
